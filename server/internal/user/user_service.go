06334af6c6f6a0b223c6b67c79ab280390217e60
Zaur Bibilov <bgwrld77@gmail.com>
2024-01-18 12:16:30 +0300

    original

---
+++ server/internal/user/user_service.go
@@ -0,0 +1,89 @@
+package user
+
+import (
+	"context"
+	"github.com/dexguitar/wskfkchat/util"
+	"github.com/golang-jwt/jwt/v4"
+	"strconv"
+	"time"
+)
+
+const secretKey = "mySecretKey"
+
+type service struct {
+	Repository
+	timeout time.Duration
+}
+
+func NewService(repository Repository) Service {
+	return &service{
+		repository,
+		time.Second * 2,
+	}
+}
+
+func (s *service) CreateUser(ctx context.Context, req *CreateUserReq) (*CreateUserRes, error) {
+	ctx, cancel := context.WithTimeout(ctx, s.timeout)
+	defer cancel()
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	u := &User{
+		Username: req.Username,
+		Email:    req.Email,
+		Password: hashedPassword,
+	}
+
+	r, err := s.Repository.CreateUser(ctx, u)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &CreateUserRes{
+		ID:       strconv.Itoa(int(r.ID)),
+		Username: r.Username,
+		Email:    r.Email,
+	}
+
+	return res, nil
+}
+
+type JWTClaims struct {
+	ID       string `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	jwt.RegisteredClaims
+}
+
+func (s *service) Login(ctx context.Context, req *LoginUserReq) (*LoginUserRes, error) {
+	ctx, cancel := context.WithTimeout(ctx, s.timeout)
+	defer cancel()
+
+	u, err := s.Repository.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	err = util.CheckPassword(u.Password, req.Password)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, JWTClaims{
+		ID:       strconv.Itoa(int(u.ID)),
+		Username: u.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    strconv.Itoa(int(u.ID)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	})
+
+	ss, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	return &LoginUserRes{accessToken: ss, ID: strconv.Itoa(int(u.ID)), Username: u.Username}, nil
+}
---
+++ server/internal/ws/hub.go
@@ -0,0 +1,74 @@
+package ws
+
+import (
+	"context"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"log"
+)
+
+type Room struct {
+	ID      string             `json:"id"`
+	Name    string             `json:"name"`
+	Clients map[string]*Client `json:"clients"`
+}
+
+type Hub struct {
+	Rooms      map[string]*Room
+	Register   chan *Client
+	Unregister chan *Client
+	Broadcast  chan *Message
+	Writer     *kafka.Writer
+}
+
+func NewHub(writer *kafka.Writer) *Hub {
+	return &Hub{
+		Rooms:      make(map[string]*Room),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Broadcast:  make(chan *Message, 5),
+		Writer:     writer,
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case cl := <-h.Register:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				r := h.Rooms[cl.RoomID]
+
+				if _, ok := r.Clients[cl.ID]; !ok {
+					r.Clients[cl.ID] = cl
+				}
+			}
+		case cl := <-h.Unregister:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				if _, ok := h.Rooms[cl.RoomID].Clients[cl.ID]; ok {
+					// produce a message saying that the client has left the room
+					if len(h.Rooms[cl.RoomID].Clients) != 0 {
+						h.Produce(context.Background(), &Message{
+							Content:  fmt.Sprintf("❌ user left the room"),
+							RoomID:   cl.RoomID,
+							Username: cl.Username,
+						})
+					}
+
+					delete(h.Rooms[cl.RoomID].Clients, cl.ID)
+					close(cl.Message)
+				}
+			}
+		}
+	}
+}
+
+func (h *Hub) Produce(ctx context.Context, m *Message) {
+	err := h.Writer.WriteMessages(ctx, kafka.Message{
+		Key:     []byte(m.RoomID),
+		Value:   []byte(m.Content),
+		Headers: []kafka.Header{{Key: "username", Value: []byte(m.Username)}},
+	})
+	if err != nil {
+		log.Printf("could not write message: %v", err)
+	}
+}
